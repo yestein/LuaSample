81124baf3fba70a4f191506b4e11a03eceff4731
Yestein <yestein86@gmail.com>
2016-07-12 16:12:45 +0800

    Eller Algorith Bug Fix

--- MazeGenerator/eller_algorithm.lua
+++ MazeGenerator/eller_algorithm.lua
@@ -40,6 +40,7 @@ local function PrintMap(data)
     end
 end
 
+local count = 20
 local function FillMaze(x, y)
     local maze_data = {
         x = x,
@@ -49,7 +50,8 @@ local function FillMaze(x, y)
         maze_data[j] = {}
         for i = 1, 2 * x + 1 do
             if i % 2 == 0 and j % 2 == 0 then
-                maze_data[j][i] = XY2Index(i, j)
+                count = count + 1
+                maze_data[j][i] = count
             else
                 maze_data[j][i] = WALL
             end
@@ -69,6 +71,22 @@ local function CanBreak(flag_a, flag_b)
 end
 
 local function GetRowHandler(maze_data, max_x, max_y, judge_break_func)
+    local set_list = {}
+    local function GetSet(index)
+        return set_list[index]
+    end
+
+    local function CreateSet(index)
+        local set = Util.GetUnionSet()
+        set.index = index
+        set_list[index] = set
+        return set
+    end
+
+    local function RemoveSet(index)
+        set_list[index] = nil
+    end
+
     local function GetRoomFlag(room_x, room_y)
         return maze_data[2 * room_y][2 * room_x]
     end
@@ -79,6 +97,22 @@ local function GetRowHandler(maze_data, max_x, max_y, judge_break_func)
 
     local function Break(room_x_a, room_y_a, room_x_b, room_y_b)
         maze_data[room_y_a + room_y_b][room_x_a + room_x_b] = CORRIDOR
+
+        local flag_a = GetRoomFlag(room_x_a, room_y_a)
+        local flag_b = GetRoomFlag(room_x_b, room_y_b)
+        local set_a = GetSet(flag_a)
+        local set_b = GetSet(flag_b)
+        if set_b then
+            set_b.ForEach(function(index)
+                local x, y = Index2XY(index)
+                SetRoomFlag(x, y, flag_a)
+                set_a.Add(index)
+            end)
+            RemoveSet(flag_b)
+        else
+            SetRoomFlag(room_x_b, room_y_b, flag_a)
+            set_a.Add(XY2Index(room_x_b, room_y_b))
+        end
     end
 
     local function IsBreak(room_y, flag_a, flag_b)
@@ -86,36 +120,30 @@ local function GetRowHandler(maze_data, max_x, max_y, judge_break_func)
     end
 
     return function(room_y)
-        local set_array = {} -- Sets array at this row
-
-        local room_2_set = {} -- Get Set Room Belongs
+        local temp_set_list = {}
         for room_x = 1, max_x do
-            local set = room_2_set[room_x]
+            local flag_a = GetRoomFlag(room_x, room_y)
+            local set = GetSet(flag_a)
             if not set then
-                set = Util.GetUnionSet()
-                table.insert(set_array, set)
+                set = CreateSet(flag_a)
                 set.Add(XY2Index(room_x, room_y))
-                room_2_set[room_x] = set
             end
+            local temp_set = temp_set_list[flag_a]
+            if not temp_set then
+                temp_set = Util.GetUnionSet()
+                temp_set_list[flag_a] = temp_set
+            end
+            temp_set.Add(XY2Index(room_x, room_y))
+
             if room_x == max_x then
                 break
             end
 
             --Break
-            local flag_a = GetRoomFlag(room_x, room_y)
             local flag_b = GetRoomFlag(room_x + 1, room_y)
             local is_break = IsBreak(room_y, flag_a, flag_b)
             if is_break then
                 Break(room_x, room_y, room_x + 1, room_y)
-                SetRoomFlag(room_x + 1, room_y, flag_a)
-                flag_b = flag_a
-            end
-
-            --Merge set
-            if flag_a == flag_b then
-                room_2_set[room_x + 1] = set
-                set.Add(XY2Index(room_x + 1, room_y))
-                SetRoomFlag(room_x + 1, room_y, flag_a)
             end
         end
 
@@ -124,13 +152,12 @@ local function GetRowHandler(maze_data, max_x, max_y, judge_break_func)
         end
 
         --Down Break
-        for _, set in ipairs(set_array) do
+        for index, set in pairs(temp_set_list) do
             local count = math.random(1, set.Count())
             local pick_list = Util.RandomPick(count, set._GetArray())
             for _, index in ipairs(pick_list) do
                 local room_x, room_y = Index2XY(index)
                 Break(room_x, room_y, room_x, room_y + 1)
-                SetRoomFlag(room_x, room_y + 1, GetRoomFlag(room_x, room_y))
             end
         end
     end
